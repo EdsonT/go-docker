e6b04dfcd890823917f4e84af1eb9325dab8d14b
Edson Tarqui <ednoosde.wob@gmail.com>
2020-04-08 00:46:10 -0400

    relativePath fix

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main()  {
 	gin.ForceConsoleColor()
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(200,gin.H{
 			"message":"pong",
 		})
